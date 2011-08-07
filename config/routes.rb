c52bd1b83bda113a8c8be53c66ef6dc03ccc3036
Tim Oakes <toakes@signalhq.com>
2011-08-06 22:15:00 -0500

    adding catchall route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+
+  # catch-all for routes that don't exist, redirect to login
+  ActionController::Routing::Routes.draw do |map|
+      map.connect ':controller/:action/:id'
+      map.connect '*path', :controller => 'login', :action => 'index'
+  end
+
 end
